54f1beb0924164ed016cb32bf7f2fba1442ac2f0
ohad <ohad@theora.com>
2020-11-19 12:43:20 +0200

    gitDiffs

--- dbBackups/backups/campaigns.data.sql
+++ dbBackups/backups/campaigns.data.sql
@@ -1,3 +1,4 @@
 INSERT INTO campaigns VALUES (1,'Sample App Campaign','ohad','app',1,24,2,4,25,'IL','veggyBanner.jpg','','http://misc.theora.com/babbani',NULL,NULL,377,133,NULL,'2020-08-01 13:19:54','ohad');
 INSERT INTO campaigns VALUES (2,'sdc','ohad','desktop',0,24,2,4,25,'IL','veggyBanner.jpg','','http://misc.theora.com/babbani',NULL,NULL,377,133,NULL,'2020-08-01 13:20:16','ohad');
 INSERT INTO campaigns VALUES (3,'High Volume','ohad','app',0,24,0.5,1.5,5,'IL','veggyBanner.jpg','','http://misc.theora.com/babbani',NULL,'17,18,19,20',377,133,NULL,'2020-08-01 13:20:33','ohad');
+INSERT INTO campaigns VALUES (8,'t21','ohad','desktop',NULL,23,3,5,20,'','babbani.jpg',NULL,'http://misc.theora.com/babbani','Array','14,15',188,250,NULL,'2020-10-28 13:10:03','ohad');
