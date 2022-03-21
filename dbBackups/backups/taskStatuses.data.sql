0eee7b5372e82ef94e9dd3c43cdbd3090299f5b2
ohad <ohad@theora.com>
2022-03-21 11:34:03 +0200

    SnapShot

--- dbBackups/backups/taskStatuses.data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO taskStatuses VALUES (1,'onHold');
-INSERT INTO taskStatuses VALUES (2,'open');
-INSERT INTO taskStatuses VALUES (3,'inProgress');
-INSERT INTO taskStatuses VALUES (4,'resolved');
-INSERT INTO taskStatuses VALUES (5,'closed');
