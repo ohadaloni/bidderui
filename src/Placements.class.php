71e7d242b6c511f21fa871685db17936dd4b63d3
ohad <ohad@theora.com>
2022-05-11 09:51:33 +0300

    gitDiffs

--- src/Placements.class.php
+++ src/Placements.class.php
@@ -14,7 +14,7 @@ class Placements extends BidderUI {
 		$tops = array_slice($placements, 0, $numTops);
 		$agoDays = PLACEMENT_OPT_WINDOW;
 		$ago = date("Y-m-d", time() - $agoDays*24*3600);
-		$minWins = PLACEMENT_MIN_WINS ;
+		$minWins = PLACEMENT_MIN_WINS;
 		$this->Mview->msg("Placements summary: showing top $numTops with at least $minWins wins since $ago");
 		$this->Mview->showTpl("placements/summary.tpl", array(
 			'rows' => $tops,
@@ -22,8 +22,8 @@ class Placements extends BidderUI {
 	}
 	/*------------------------------------------------------------*/
 	private function byPpmDesc($row1, $row2) {
-		$diff =  $row2['ppm'] - $row1['ppm'];
-		$order = $diff > 0 ? 1 : ( $diff < 0 ? -1 : 0 ) ;
+		$diff = $row2['ppm'] - $row1['ppm'];
+		$order = $diff > 0 ? 1 : ( $diff < 0 ? -1 : 0 );
 		return($order);
 	}
 	/*------------------------------------------------------------*/
